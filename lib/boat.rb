f1c175572a17946680ba3a8d45593ea9ee3035be
Ana Perez Santos <a.jperez@yahoo.com>
2020-02-18 19:45:02 -0700

    Test it initializes with attributes for type and price_per_hour

--- lib/boat.rb
+++ lib/boat.rb
@@ -1,6 +1,8 @@
 class Boat
+  attr_reader :type, :price_per_hour
 
-  def initialize(type, price_per_hour)
+  def initialize(type, price)
     @type = type
+    @price_per_hour = price
   end
 end
--- test/boat_test.rb
+++ test/boat_test.rb
@@ -9,4 +9,11 @@ class BoatTest < Minitest::Test
 
     assert_instance_of Boat, kayak
   end
+
+  def test_it_has_attributes
+    kayak = Boat.new(:kayak, 20)
+
+    assert_equal :kayak, kayak.type
+    assert_equal 20, kayak.price_per_hour
+  end
 end
