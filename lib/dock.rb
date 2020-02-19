5e05c9fbb0fd6f679a3cdda376a0eae0eeea631d
Ana Perez Santos <a.jperez@yahoo.com>
2020-02-18 20:23:53 -0700

    Test it initializes with attributes for name and max_rental_time

--- lib/dock.rb
+++ lib/dock.rb
@@ -1,6 +1,8 @@
 class Dock
+  attr_reader :name, :max_rental_time
 
-  def initialize(name, max_rental_hours)
-
+  def initialize(name, rental_hour_limit)
+    @name = name
+    @max_rental_time = rental_hour_limit
   end
 end
--- test/dock_test.rb
+++ test/dock_test.rb
@@ -11,4 +11,11 @@ class DockTest < Minitest::Test
 
     assert_instance_of Dock, dock
   end
+
+  def test_it_has_attributes
+    dock = Dock.new("The Rowing Dock", 3)
+
+    assert_equal "The Rowing Dock", dock.name
+    assert_equal 3, dock.max_rental_time
+  end
 end
