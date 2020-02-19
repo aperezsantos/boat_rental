8b082a5bf64f80729c95ab971d5a13e4c5825bca
Ana Perez Santos <a.jperez@yahoo.com>
2020-02-18 20:08:01 -0700

    Create Renter test/class files, pass first test

---
+++ lib/renter.rb
@@ -0,0 +1,7 @@
+class Renter
+
+  def initialize(name, cc_number)
+    @name = name
+    @cc_number = cc_number
+  end
+end
---
+++ test/renter_test.rb
@@ -0,0 +1,13 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/renter'
+require './lib/boat'
+
+class RenterTest < Minitest::Test
+
+  def test_it_exists
+    renter = Renter.new("Patrick Star", "4242424242424242")
+
+    assert_instance_of Renter, renter
+  end
+end
