85b351bac87aabffc46c9e8909f59857f2045a29
Ana Perez Santos <a.jperez@yahoo.com>
2020-02-18 20:10:54 -0700

    Initialize instance with attributes, pass test

--- lib/renter.rb
+++ lib/renter.rb
@@ -1,7 +1,8 @@
 class Renter
+  attr_reader :name, :credit_card_number
 
   def initialize(name, cc_number)
     @name = name
-    @cc_number = cc_number
+    @credit_card_number = cc_number
   end
 end
